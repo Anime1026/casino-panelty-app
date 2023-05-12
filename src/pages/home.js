ae224d4cc0af88db32ee823ce74480882e559226
Anime1026 <sasuke031026@gmail.com>
2023-05-12 10:59:00 +0300

    update

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,20 +1,39 @@
 import React, { useState, useEffect } from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
+import logo from "../logo.png";
 
 const unityContext = new UnityContext({
-    loaderUrl: "build/Build/build.loader.js",
-    dataUrl: "build/Build/build.data",
-    frameworkUrl: "build/Build/build.framework.js",
-    codeUrl: "build/Build/build.wasm"
+    loaderUrl: "build/Build/WebGL.loader.js",
+    dataUrl: "build/Build/WebGL.data.unityweb",
+    frameworkUrl: "build/Build/WebGL.framework.js.unityweb",
+    codeUrl: "build/Build/WebGL.wasm.unityweb"
 });
 
 function Home() {
+    const [percent, setPercent] = useState(false);
+    const [progress, setProgress] = useState(0);
+    useEffect(() => {
+        unityContext.on("progress", (progress) => {
+            setProgress(progress * 1.11 * 100);
+            if (progress === 1) {
+                setPercent(true);
+            }
+        })
+    }, [])
     return (
         <div>
+            {!percent && (
+                <div className="wait">
+                    <img src={logo}></img>
+                    <div className="percent">
+                        <div className="progress" style={{ width: `${progress}%` }}></div>
+                    </div>
+                </div>
+            )}
             <Unity
                 unityContext={unityContext}
                 matchWebGLToCanvasSize={true}
-                style={{ height: "100vh" }}
+                style={{ height: "99vh" }}
             />
         </div>
     );
